94374b5db9158648f0d9c30acaa4f3529473f2cd
James Muturi <james.muturi@andela.com>
2016-11-04 07:59:51 +0300

    Ch01 - Basic blog application

---
+++ mysite/blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
+from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 10
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
