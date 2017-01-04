99068359c2961050b1fe16b11400901a253f2b6c
James Muturi <james.muturi@andela.com>
2017-01-04 12:22:32 +0300

    Create image detail view

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^create/$', views.image_create, name='create'),
+    url(
+        r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.image_detail,
+        name='detail'
+    ),
 ]
