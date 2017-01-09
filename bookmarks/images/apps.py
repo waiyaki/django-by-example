6faa0347ad57bfa0b2371f3853bf8f452b644488
James Muturi <james.muturi@andela.com>
2017-01-09 15:30:21 +0300

    Work with signals to update image total likes

--- bookmarks/images/apps.py
+++ bookmarks/images/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class ImagesConfig(AppConfig):
     name = 'images'
+    verbose_name = 'Image bookmarks'
+
+    def ready(self):
+        # import signal handlers
+        import images.signals
