bf95194135f222f3ca025a77d085ed8421fd9fb0
James Muturi <james.muturi@andela.com>
2017-01-12 22:16:19 +0300

    Store shopping carts in sessions
    - Create carts in session
    - Display shopping cart
    - Add products to shopping cart
    - Update product quantities in carts

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
