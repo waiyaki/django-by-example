8f98dadabd73e48e9aa97aa22036957d4bb51155
James Muturi <james.muturi@andela.com>
2017-01-14 21:20:44 +0300

    Create customer orders

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import OrderCreateForm
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order, product=item['product'],
+                    price=item['price'], quantity=item['quantity']
+                )
+
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    context = {'cart': cart, 'form': form}
+    return render(request, 'orders/order/create.html', context)
