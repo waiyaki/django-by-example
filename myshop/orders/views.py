18902e2f43c02c4e93af3be5d1e4a149b3cec6ec
James Muturi <james.muturi@andela.com>
2017-01-16 14:29:45 +0300

    Launch asynchronous tasks with Celery

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 
 
 def order_create(request):
@@ -18,6 +19,8 @@ def order_create(request):
                 )
 
             cart.clear()
+            # launch async task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
